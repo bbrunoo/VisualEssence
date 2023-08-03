29a7c54692139b4138253fb324de702dc2960b8b
21011 <21011@LAB1MAQ05>
2023-08-03 11:05:46 -0300

    Add project files.

---
+++ VisualEssence/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿
+const [state, setState] = useState(false);
+const toggle = () => {
+    setState(!state);
+}
+
+const sobre = useRef(null);
+const ongs = useRef(null);
+const jogos = useRef(null);
+const contato = useRef(null);
+
+const scrollToSection = (elementRef) => {
+    window.scrollTo({
+        top: elementRef.current.offsetTop,
+        behavior: 'smooth'
+    })
